fe381e1ba2d7958d801e3c5d51044eda5016ae5f
l_sivan <907616591@qq.com>
2017-08-08 16:06:04 +0800

    换成ferry踩坑过的xml解析方式

--- src/server/kit/wechat/pay/main.go
+++ src/server/kit/wechat/pay/main.go
@@ -14,6 +14,8 @@ import (
 	"time"
 	"maizuo.com/soda/erp/api/src/server/common"
 	"maizuo.com/soda/erp/api/src/server/kit/functions"
+	"maizuo.com/soda/erp/api/src/server/kit/util"
+	"bytes"
 )
 
 type WechatPayKit struct {
@@ -22,22 +24,40 @@ type WechatPayKit struct {
 /**
 微信支付计算签名的函数
 */
-func (self *WechatPayKit) CreateSign(mReq map[string]interface{}) (string) {
+func (self *WechatPayKit) CreateSign(m interface{}) (string) {
 	apiKey := viper.GetString("resource.pay.wechat.api-key")
 	//apiKey := os.Getenv("APIKEY")
 	common.Logger.Debugln("微信支付签名计算, API KEY:", apiKey)
 	//STEP 1, 对key进行升序排序.
 	sorted_keys := make([]string, 0)
-	for k, _ := range mReq {
-		sorted_keys = append(sorted_keys, k)
+
+	switch mValue := m.(type) {
+	case map[string]interface{}:
+		for k, _ := range mValue {
+			sorted_keys = append(sorted_keys, k)
+		}
+	case map[string]string:
+		for k, _ := range mValue {
+			sorted_keys = append(sorted_keys, k)
+		}
+	default:
+		return ""
 	}
 	sort.Strings(sorted_keys)
 	//STEP2, 对key=value的键值对用&连接起来，略过空值
 	var signStrings string
 	for _, k := range sorted_keys {
-		fmt.Printf("k=%v, v=%v\n", k, mReq[k])
-		value := fmt.Sprintf("%v", mReq[k])
-		if value != "" {
+		value := ""
+		switch mValue := m.(type) {
+		case map[string]interface{}:
+			//fmt.Printf("k=%v, v=%v\n", k, mValue[k])
+			value = fmt.Sprintf("%v", mValue[k])
+		case map[string]string:
+			//fmt.Printf("k=%v, v=%v\n", k, mValue[k])
+			value = fmt.Sprintf("%v", mValue[k])
+		}
+
+		if value != "" && k != "sign" {
 			signStrings = signStrings + k + "=" + value + "&"
 		}
 	}
@@ -46,6 +66,7 @@ func (self *WechatPayKit) CreateSign(mReq map[string]interface{}) (string) {
 		signStrings = signStrings + "key=" + apiKey
 	}
 	//STEP4, 进行MD5签名并且将所有字符转为大写.
+	common.Logger.Debugln("======待加密串：", signStrings)
 	md5Ctx := md5.New()
 	md5Ctx.Write([]byte(signStrings))
 	cipherStr := md5Ctx.Sum(nil)
@@ -56,15 +77,15 @@ func (self *WechatPayKit) CreateSign(mReq map[string]interface{}) (string) {
 /**
 签名校验方法
 */
-func (self *WechatPayKit) VerifySign(data map[string]interface{}, sign string) bool {
+func (self *WechatPayKit) VerifySign(data interface{}, sign string) bool {
 	_sign := self.CreateSign(data)
 	common.Logger.Debugln("计算出来的sign: %v", _sign)
 	common.Logger.Debugln("微信通知sign: %v", sign)
 	if sign == _sign {
-		common.Logger.Debugln("签名校验通过!")
+		common.Logger.Debugln("签名校验通过=====")
 		return true
 	}
-	common.Logger.Debugln("签名校验失败!")
+	common.Logger.Debugln("签名校验失败=====")
 	return false
 }
 
@@ -114,6 +135,7 @@ func (self *WechatPayKit) CreateNativePayURL(tradeNo string) string {
 统一下单接口
 */
 func (self *WechatPayKit) CreateUnifiedOrder(unifyOrderRequest *UnifyOrderRequest) (string, error) {
+	respMap := make(map[string]string, 0)
 	appId := viper.GetString("resource.pay.wechat.app-id")
 	//appId := os.Getenv("APPID")
 	notifyUrl := viper.GetString("resource.pay.wechat.notifty-url")
@@ -137,17 +159,16 @@ func (self *WechatPayKit) CreateUnifiedOrder(unifyOrderRequest *UnifyOrderReques
 	m["nonce_str"] = unifyOrderRequest.NonceStr
 	m["product_id"] = unifyOrderRequest.ProductId
 	m["openid"] = unifyOrderRequest.OpenId
+	m["attach"] = unifyOrderRequest.Attach
 	common.Logger.Debugln("openid=======", unifyOrderRequest.OpenId)
 	unifyOrderRequest.Sign = self.CreateSign(m)
 	requestBytes, err := xml.Marshal(unifyOrderRequest)
 	if err != nil {
-		common.Logger.Warningln("以xml形式编码发送错误, 原因:", err.Error())
+		common.Logger.Warningln("以xml形式编码发送错误, 原因:", err.Error(), " tradeNo:", unifyOrderRequest.OutTradeNo)
 		return "", err
 	}
 	reqStr := string(requestBytes)
 	reqStr = strings.Replace(reqStr, "UnifyOrderRequest", "xml", -1)
-
-	common.Logger.Debugln("统一下单接口请求参数:", reqStr)
 
 	requestBytes = []byte(reqStr)
 	response, err := grequests.Post(unifiedorderUrl, &grequests.RequestOptions{
@@ -157,38 +178,37 @@ func (self *WechatPayKit) CreateUnifiedOrder(unifyOrderRequest *UnifyOrderReques
 			"Content-Type": "application/xml;charset=utf-8",
 		},
 	})
+	common.Logger.Warningln("请求统一下单接口response：", response.String())
 	if err != nil {
-		common.Logger.Warningln("请求微信支付统一下单接口发送错误, 原因:", err.Error())
+		common.Logger.Warningln("请求微信支付统一下单接口发送错误, 原因:", err.Error(), " tradeNo:", unifyOrderRequest.OutTradeNo)
 		return "", err
 	}
-	common.Logger.Infoln(response.String())
-
-	xmlResp := UnifyOrderResponse{}
-	err = xml.Unmarshal(response.Bytes(), &xmlResp)
+	respMap, err = util.DecodeXMLToMap(bytes.NewReader(response.Bytes()))
 	if err != nil {
-		common.Logger.Warningln("解析xml形式编码错误, 原因:", err.Error())
+		common.Logger.Warningln("解析xml形式编码错误, 原因:", err.Error(), " tradeNo:", unifyOrderRequest.OutTradeNo)
 		return "", err
 	}
-	if xmlResp.ReturnCode == "FAIL" {
-		common.Logger.Warningln("微信支付统一下单不成功，原因:", xmlResp.ReturnCode)
+	if respMap["return_code"] == "FAIL" {
+		common.Logger.Warningln("微信支付统一下单不成功，原因:", respMap["return_code"], " tradeNo:", unifyOrderRequest.OutTradeNo)
 		return "", err
 	}
 
 	//拿prepayId要判断returncode和resultcode还有prepayid
-	common.Logger.Warningln("微信支付统一下单成功，预支付单号:", xmlResp.PrepayId)
-	return xmlResp.PrepayId, nil
+	common.Logger.Warningln("微信支付统一下单成功，预支付单号:", respMap["prepay_id"])
+	return respMap["prepay_id"], nil
 }
 
 /**
 查询订单
 */
-func (self *WechatPayKit) CheckTrade(tradeNo string) (*InitiativeResponse, error) {
+func (self *WechatPayKit) CheckTrade(tradeNo string) (*map[string]string, error) {
 	appId := viper.GetString("resource.pay.wechat.app-id")
 	mchId := viper.GetString("resource.pay.wechat.mch-id")
 	initiativeUrl := viper.GetString("resource.pay.wechat.initiative-url")
-	xmlResp := InitiativeResponse{}
+	//xmlResp := InitiativeResponse{}
 	nonceStr := self.CreateNonceStr(32)
 	m := make(map[string]interface{}, 0)
+	respMap := make(map[string]string, 0)
 
 	m["appid"] = appId
 	m["mch_id"] = mchId
@@ -204,8 +224,8 @@ func (self *WechatPayKit) CheckTrade(tradeNo string) (*InitiativeResponse, error
 	}
 	requestBytes, err := xml.Marshal(initiativeRequest)
 	if err != nil {
-		common.Logger.Warningln("以xml形式编码发送错误, 原因:", err.Error())
-		return nil, err
+		common.Logger.Warningln("以xml形式编码发送错误, 原因:", err.Error(), " tradeNo:", tradeNo)
+		return &respMap, err
 	}
 	reqStr := string(requestBytes)
 	reqStr = strings.Replace(reqStr, "InitiativeRequest", "xml", -1)
@@ -217,36 +237,26 @@ func (self *WechatPayKit) CheckTrade(tradeNo string) (*InitiativeResponse, error
 		},
 	})
 	if err != nil {
-		common.Logger.Warningln("请求微信查询订单接口发送错误, 原因:", err.Error())
-		return nil, err
+		common.Logger.Warningln("请求微信查询订单接口发送错误, 原因:", err.Error(), " tradeNo:", tradeNo)
+		return &respMap, err
 	}
 
-	err = xml.Unmarshal(response.Bytes(), &xmlResp)
+	respMap, err = util.DecodeXMLToMap(bytes.NewReader(response.Bytes()))
 	if err != nil {
-		common.Logger.Warningln("解析xml形式编码错误, 原因:", err.Error())
-		return nil, err
+		common.Logger.Warningln("解析xml形式编码错误, 原因:", err.Error(), " tradeNo:", tradeNo)
+		return &respMap, err
 	}
-
-	return &xmlResp, nil
+	return &respMap, nil
 
 }
 
-func (self *WechatPayKit) Refund(refundRequest *RefundRequest) (*RefundResponse, error) {
+func (self *WechatPayKit) Refund(refundRequest *RefundRequest) (*map[string]string, error) {
 	wechatPayKit := &WechatPayKit{}
+	respMap := make(map[string]string, 0)
 	refundUrl := viper.GetString("resource.pay.wechat.refund-url")
 	mchId := viper.GetString("resource.pay.wechat.mch-id")
 	appId := viper.GetString("resource.pay.wechat.app-id")
 	m := make(map[string]interface{}, 0)
-	/*refundRequest := &pay.RefundRequest{
-		AppId: appId,
-		MchId: mchId,
-		NonceStr: wechatPayKit.CreateNonceStr(32),
-		OutTradeNo: trade.TradeId,
-		OutRefundNo: order.GenerateIdByMobile(trade.Mobile),
-		TotalFee: trade.Value,
-		RefundFee: trade.Value,
-		OpUserId: mchId,
-	}*/
 	m["appid"] = appId
 	m["mch_id"] = mchId
 	m["nonce_str"] = refundRequest.NonceStr
@@ -279,28 +289,61 @@ func (self *WechatPayKit) Refund(refundRequest *RefundRequest) (*RefundResponse,
 	}
 	common.Logger.Infoln(response.String())
 
-	xmlResp := &RefundResponse{}
-	err = xml.Unmarshal(response.Bytes(), &xmlResp)
+	respMap, err = util.DecodeXMLToMap(bytes.NewReader(response.Bytes()))
 	if err != nil {
-		common.Logger.Warningln("解析xml形式编码错误, 原因:", err.Error())
+		common.Logger.Warningln("解析xml形式编码错误, 原因:", err.Error(), "tradeNo:", refundRequest.OutTradeNo)
 		return nil, err
 	}
-	return xmlResp, nil
+	return &respMap, nil
 }
 
-func (self *WechatPayKit) UnifiedOrderResponse(nativePayResponse *NativePayResponse) string {
+
+func (self *WechatPayKit) BatchPay(batchPayRequest *BatchPayRequest) (*map[string]string, error) {
+	batchPayUrl := viper.GetString("resource.pay.wechat.batchpay-url")
+	mchId := viper.GetString("resource.pay.wechat.mch-id")
+	appId := viper.GetString("resource.pay.wechat.app-id")
+	checkName := viper.GetString("resource.pay.wechat.check_name")
+	ip := viper.GetString("resource.pay.ip")
 	m := make(map[string]interface{}, 0)
-	m["result_code"] = nativePayResponse.ResultCode
-	m["return_code"] = nativePayResponse.ReturnCode
-	m["return_msg"] = nativePayResponse.ReturnMsg
-	m["appid"] = nativePayResponse.AppId
-	m["mch_id"] = nativePayResponse.MchId
-	m["nonce_str"] = nativePayResponse.NonceStr
-	m["prepay_id"] = nativePayResponse.PrepayId
-	m["err_code_des"] = nativePayResponse.ErrCodeDes
-	nativePayResponse.Sign = self.CreateSign(m)
-	respBytes, _ := xml.Marshal(nativePayResponse)
-	xmlResult := strings.Replace(string(respBytes), "NativePayResponse", "xml", -1)
-	fmt.Println(xmlResult)
-	return xmlResult
+
+	m["appid"] = appId
+	m["mch_id"] = mchId
+	m["nonce_str"] = self.CreateNonceStr(32)
+	m["partner_trade_no"] = batchPayRequest.PartnerTradeNo
+	m["desc"] = batchPayRequest.Desc
+	m["amount"] = batchPayRequest.Amount
+	m["check_name"] = checkName
+	m["re_user_name"] = batchPayRequest.ReUserName
+	m["spbill_create_ip"] = ip
+	m["openid"] = batchPayRequest.Openid
+	batchPayRequest.Sign = self.CreateSign(m)
+	requestBytes, err := xml.Marshal(batchPayRequest)
+	if err != nil {
+		common.Logger.Warningln("以xml形式编码发送错误, 原因:", err.Error())
+		return nil, err
+	}
+	reqStr := string(requestBytes)
+	reqStr = strings.Replace(reqStr, "BatchPayRequest", "xml", -1)
+
+	common.Logger.Debugln("微信企业支付接口请求参数:", reqStr)
+
+	requestBytes = []byte(reqStr)
+	response, err := grequests.Post(batchPayUrl, &grequests.RequestOptions{
+		XML: requestBytes,
+		Headers: map[string]string{
+			"Accept":       "application/xml",
+			"Content-Type": "application/xml;charset=utf-8",
+		},
+	})
+	if err != nil {
+		common.Logger.Warningln("请求微信企业支付接口发送错误, 原因:", err.Error())
+		return nil, err
+	}
+
+	respMap, err := util.DecodeXMLToMap(bytes.NewReader(response.Bytes()))
+	if err != nil {
+		common.Logger.Warningln("解析xml形式编码错误, 原因:", err.Error())
+		return &respMap, err
+	}
+	return &respMap, nil
 }
