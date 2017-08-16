6f0ba57974378beb76db01a7e5dc19b28b929be9
l_sivan <907616591@qq.com>
2017-08-16 18:22:31 +0800

    账单与日账单返回时增加realname字段与修改name的内容

--- src/server/model/main.go
+++ src/server/model/main.go
@@ -1,18 +1,10 @@
 package model
 
 func (self *Bill) Mapping(user *User) map[string]interface{} {
-	payName := ""
-	switch self.AccountType {
-	case 1:
-		payName = "支付宝"
-	case 2:
-		payName = "微信"
-
-	}
 	settledAt := ""
 	if &self.SettledAt != nil {
 		settledAt = ""
-	}else{
+	} else {
 		settledAt = self.SettledAt.Format("2006-01-02T15:04:05+00:00")
 	}
 	return map[string]interface{}{
@@ -25,9 +17,10 @@ func (self *Bill) Mapping(user *User) map[string]interface{} {
 			"mobile": user.Mobile,
 		},
 		"account": map[string]interface{}{
-			"type":    self.AccountType,
-			"payName": payName,
-			"name":    self.AccountName,
+			"type":     self.AccountType,
+			"payName":  self.AccountName,
+			"name":     self.Account,
+			"realName": self.RealName,
 		},
 		"totalAmount": self.TotalAmount,
 		"amount":      self.Amount,
@@ -53,7 +46,8 @@ func (self *DailyBill) Mapping(user *User) map[string]interface{} {
 		"account": map[string]interface{}{
 			"type":    self.AccountType,
 			"payName": self.AccountName,
-			"name":    self.RealName,
+			"name":    self.Account,
+			"realName":self.RealName,
 		},
 		"totalAmount": self.TotalAmount,
 		"status":      self.Status,
@@ -61,28 +55,4 @@ func (self *DailyBill) Mapping(user *User) map[string]interface{} {
 		"id":          self.ID,
 	}
 }
-//func (self *DailyBill) MappingDetails(device *Device, ticket *soda.Ticket) map[string]interface{} {
-//	return map[string]interface{}{
-//		"createdAt": self.CreatedAt,
-//		"updatedAt": self.UpdatedAt,
-//		"settledAt": self.SettledAt,
-//		"billAt":    self.BillAt,
-//		"user": map[string]interface{}{
-//			"id":     ticket.UserId,
-//			"mobile": ticket.Mobile,
-//		},
-//		"device": {
-//			"serial":  device.SerialNumber,
-//			"address": device.Address,
-//		},
-//		"pay": {
-//			"type":   ticket.PaymentId,
-//			"amount": self.TotalAmount,
-//		},
-//		"totalAmount": self.TotalAmount,
-//		"status":      ticket.Status,
-//		"hasSettled":  self.SettledAt,
-//		"id":          self.ID,
-//		"type":        ticket.DeviceMode,
-//	}
-//}
+
